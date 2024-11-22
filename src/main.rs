218e9ea8761d23d604a432d027ecc42f40f64ff9
Richard Barnes <rlb@ipv.sx>
2024-11-22 09:27:25 -0500

    Start setting up the screen

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use defmt::info;
 use embassy_executor::Spawner;
 use embassy_stm32::{
     gpio::{Level, Output, Speed},
+    spi::Spi,
     time::Hertz,
     Config,
 };
@@ -13,8 +14,7 @@ use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    info!("Hello World!");
-
+    // Configure the device and get a handle to its capabilities
     let mut config = Config::default();
     {
         use embassy_stm32::rcc::*;
@@ -41,14 +41,51 @@ async fn main(_spawner: Spawner) {
     }
     let p = embassy_stm32::init(config);
 
+    // Configure the LED
     let mut led = Output::new(p.PC5, Level::High, Speed::Low);
 
+    // Configure SPI1
+    let spi1 = {
+        use embassy_stm32::spi::{Config, Spi};
+        let mut config = Config::default(); // TODO
+
+        Spi::new_txonly(
+            p.SPI1, // SPI peripheral
+            p.PA5,  // Clock pin
+            p.PA7,  // MOSI pin
+            config,
+        )
+    };
+
+    let mut screen = Screen {
+        spi: spi1,
+        cs: p.PC14,
+        dc: p.PC13,
+        rst: p.PB9,
+        bl: p.PB8,
+    };
+
+    // Do stuff
     loop {
         fib_test::<u32>(34, &mut led).await;
         fib_test::<u64>(34, &mut led).await;
         fib_test::<f32>(32, &mut led).await;
         fib_test::<f64>(32, &mut led).await;
     }
+}
+
+struct Screen<CS, DC, RST, BL>
+where
+    CS: Pin,  // C 14
+    DC: Pin,  // C 13
+    RST: Pin, // B 9
+    BL: Pin,  // B 8
+{
+    spi: Spi,
+    cs: CS,
+    dc: DC,
+    rst: RST,
+    bl: BL,
 }
 
 async fn fib_test<'d, T>(n: usize, led: &mut Output<'d>)
